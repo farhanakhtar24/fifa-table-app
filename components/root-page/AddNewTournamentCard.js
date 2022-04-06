67088c9b6dbd96806081f8b48d834ebdc4715a61
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-04-07 03:48:35 +0530

    carousel added , react downgraded to 17.0.2

--- components/root-page/AddNewTournamentCard.js
+++ components/root-page/AddNewTournamentCard.js
@@ -19,7 +19,7 @@ const AddNewTournamentCard = () => {
     return (
         <Link href={ '/new-tournament' }>
             <a>
-                <div className='border-4 border-primary_dark_blue rounded-lg h-full w-full text-3xl font-bold
+                <div className='border-4 border-primary_dark_blue rounded-2xl h-full w-full text-3xl font-bold
                 flex flex-col items-center justify-center gap-4 cursor-pointer'>
                     NEW
                     <div className='m-5'>
--- components/root-page/TournamentsBox.js
+++ components/root-page/TournamentsBox.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import ContentBox from '../UI/ContentBox';
 import AddNewTournamentCard from './AddNewTournamentCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 
 const TournamentsBox = () => {
 
     return (
         <ContentBox heading={ 'tournaments' } headingStyle={ 'text-4xl font-bold' }>
-            <div className='w-full h-full grid grid-cols-5 gap-10 px-14 pb-14'>
-                <AddNewTournamentCard />
+            <div className='w-full h-full pb-12 px-12 grid grid-cols-5 gap-8'>
+                <div className='col-span-1'>
+                    <AddNewTournamentCard />
+                </div>
+                <div className='col-span-4'>
+                    <Swiper
+                        slidesPerView={ 4 }
+                        spaceBetween={ 30 }
+                        pagination={ {
+                            clickable: true,
+                        } }
+                        navigation={ true }
+                        modules={ [Navigation, Pagination] }
+                        className="w-full h-full"
+                    >
+                        <SwiperSlide>
+                            <AddNewTournamentCard />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <AddNewTournamentCard />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <AddNewTournamentCard />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <AddNewTournamentCard />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <AddNewTournamentCard />
+                        </SwiperSlide>
+
+                    </Swiper>
+                </div>
             </div>
         </ContentBox>
     )
